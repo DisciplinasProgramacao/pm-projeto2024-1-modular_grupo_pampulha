8a6c4bce5d4670caf5d7cb622591baec84d49d51
FelipeParreiras <77993189+FelipeParreiras@users.noreply.github.com>
2024-06-22 20:04:23 -0300

    Update Restaurante.java

--- codigo/Restaurante.java
+++ codigo/Restaurante.java
@@ -18,13 +18,13 @@ public class Restaurante {
 
     private void inicializarMesas() {
         for (int i = 1; i <= 4; i++) {
-            mesas.put(i, new Mesa(4));
+            mesas.put(i, new MesaPequena());
         }
         for (int i = 5; i <= 8; i++) {
-            mesas.put(i, new Mesa(6));
+            mesas.put(i, new MesaMedia());
         }
         for (int i = 9; i <= 10; i++) {
-            mesas.put(i, new Mesa(8));
+            mesas.put(i, new MesaGrande());
         }
     }
 
@@ -62,6 +62,33 @@ public class Restaurante {
         return false;
     }
 
+    public boolean realizarMenuFechado(int numeroMesa, Item comida, Item bebida1, Item bebida2) {
+        Mesa mesa = mesas.get(numeroMesa);
+        if (mesa != null && mesa.isOcupada()) {
+            // Cria os pedidos para comida e bebidas
+            Pedido comidaPedido = new Pedido(new ItemMenu(0, comida.getNome(), comida.getPreco()));
+            Pedido bebidaPedido1 = new Pedido(new ItemMenu(0, bebida1.getNome(), bebida1.getPreco()));
+            Pedido bebidaPedido2 = new Pedido(new ItemMenu(0, bebida2.getNome(), bebida2.getPreco()));
+
+            // Adiciona os pedidos à mesa
+            mesa.adicionarPedido(comidaPedido);
+            mesa.adicionarPedido(bebidaPedido1);
+            mesa.adicionarPedido(bebidaPedido2);
+
+            // Calcula o valor total e aplica o desconto
+            double valorTotal = comida.getPreco() + bebida1.getPreco() + bebida2.getPreco();
+            double desconto = valorTotal - 32.0;
+
+            // Cria um pedido de desconto para ajustar o valor total para 32
+            Pedido descontoPedido = new Pedido(new ItemMenu(0, "Desconto Menu Fechado", -desconto));
+            mesa.adicionarPedido(descontoPedido);
+
+            return true;
+        }
+        return false;
+    }
+
+
     public String listarItensCardapio() {
         return cardapio.listarItens();
     }
@@ -70,7 +97,23 @@ public class Restaurante {
         return mesas;
     }
 
+    public Cardapio getCardapio() {
+        return cardapio;
+    }
+
     public String getNomeRestaurante() {
         return nomeRestaurante;
+    }
+
+    // Método para listar mesas ocupadas com seus responsáveis
+    public String listarMesasComResponsaveis() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("### Mesas Ocupadas ###\n");
+        for (Mesa mesa : mesas.values()) {
+            if (mesa.isOcupada()) {
+                sb.append("Mesa ").append(mesa.getIdMesa()).append(" - Responsável: ").append(mesa.getCliente().getNome()).append("\n");
+            }
+        }
+        return sb.toString();
     }
 }
